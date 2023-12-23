e198ffb52be24214c8aadb68408bc12d9382bf6e
lds2292 <lds2292@gmail.com>
2023-12-24 02:03:26 +0900

    JobParameter에 관한 내용

--- src/main/java/com/browngoo/springbatch/JobParametersConfiguration.java
+++ src/main/java/com/browngoo/springbatch/JobParametersConfiguration.java
@@ -2,6 +2,7 @@ package com.browngoo.springbatch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -9,9 +10,11 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 @Configuration
 @RequiredArgsConstructor
-public class JobInstanceConfiguration {
+public class JobParametersConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -27,7 +30,16 @@ public class JobInstanceConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-            .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED).build();
+            .tasklet((contribution, chunkContext) -> {
+                JobParameters jobParameters = contribution.getStepExecution().getJobParameters();
+                System.out.println("jobParameters.getString(\"name\") = " + jobParameters.getString("name"));;
+                System.out.println("jobParameters.getLong(\"seq\") = " + jobParameters.getLong("seq"));;
+                System.out.println("jobParameters.getDate(\"date\") = " + jobParameters.getDate("date"));;
+                System.out.println("jobParameters.getDouble(\"age\") = " + jobParameters.getDouble("age"));;
+
+                Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+                return RepeatStatus.FINISHED;
+            }).build();
     }
 
     @Bean
--- src/main/java/com/browngoo/springbatch/JobRunner.java
+++ src/main/java/com/browngoo/springbatch/JobRunner.java
@@ -8,6 +8,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+
 @Component
 public class JobRunner implements ApplicationRunner {
 
@@ -21,7 +24,11 @@ public class JobRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        JobParameters jobParameters = new JobParametersBuilder().addString("name", "user2")
+        JobParameters jobParameters = new JobParametersBuilder()
+            .addString("name", "user2")
+            .addLong("seq", 2L)
+            .addDate("date", new Date())
+            .addDouble("age", 16.5)
             .toJobParameters();
         jobLauncher.run(job, jobParameters);
     }
