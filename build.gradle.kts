1a116720c8712a9d24cc174a5fe5f2223cc6e60c
Eden.Lee <deoksoo.lee@smartfoodnet.co.kr>
2023-12-23 23:39:01 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.7.18"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.browngoo"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("com.h2database:h2:2.2.224")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.batch:spring-batch-test")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
