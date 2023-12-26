680f1b0f508e71094dba4b63c12e244d78b33b70
lds2292 <lds2292@gmail.com>
2023-12-26 23:22:15 +0900

    PreventReStart

--- src/main/java/com/browngoo/springbatch/PreventRestartConfiguration.java
+++ src/main/java/com/browngoo/springbatch/PreventRestartConfiguration.java
@@ -1,6 +1,8 @@
 package com.browngoo.springbatch;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -13,7 +15,7 @@ import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
-public class ValidatorConfiguration {
+public class PreventRestartConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -24,7 +26,7 @@ public class ValidatorConfiguration {
             .start(step1())
             .next(step2())
             .next(step3())
-            .validator(new CustomJobParameterValidator())
+            .preventRestart()
             .build();
     }
 
@@ -40,12 +42,18 @@ public class ValidatorConfiguration {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-            .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED).build();
+            .tasklet((contribution, chunkContext) -> {
+                return RepeatStatus.FINISHED;
+            }).build();
     }
 
     @Bean
     public Step step3() {
         return stepBuilderFactory.get("step3")
-            .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED).build();
+            .tasklet((contribution, chunkContext) -> {
+                chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+                contribution.getStepExecution().setExitStatus(ExitStatus.STOPPED);
+                return RepeatStatus.FINISHED;
+            }).build();
     }
 }
